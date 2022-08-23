608c5487f360a4a34affb53c43cbbab0fcc955e7
Makar_Jv <makar_igor1997@mail.ru>
2022-08-23 19:24:21 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let pswrd = document.getElementById('pswrd')
+let toggleBtn = document.getElementById('toggleBtn')
+
+toggleBtn.onclick = function() {
+    if (pswrd.type === 'password'){
+        pswrd.setAttribute('type', 'text')
+        toggleBtn.classList.add('hide')
+    } else {
+        pswrd.setAttribute('type', 'password')
+        toggleBtn.classList.remove('hide')
+    }
+}
